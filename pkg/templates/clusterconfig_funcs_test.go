2f66cb8eaf2fa34cf7dc144a3ed8076ccef6017b
Matt Prahl <mprahl@users.noreply.github.com>
2021-08-24 17:46:04 -0400

    Disable fromClusterClaim when namespace restrictions are set (#12)
    In commit 612affb, an attempt was made to restrict fromClusterClaim to a
    specific namespace when the namespace is restricted (LookupNamespace).
    The issue is that ClusterClaim is a cluster resource and not a namespace
    resource so this approach did not work.
    
    This commit makes it so that fromClusterClaim raises an error when the
    namespace is restricted (LookupNamespace).
    
    Signed-off-by: mprahl <mprahl@users.noreply.github.com>

---
+++ pkg/templates/clusterconfig_funcs_test.go
@@ -0,0 +1,30 @@
+// Copyright Contributors to the Open Cluster Management project
+
+package templates
+
+import (
+	"testing"
+
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/kubernetes/fake"
+	"k8s.io/client-go/rest"
+)
+
+func TestFromClusterClaimNsError(t *testing.T) {
+	t.Parallel()
+	var kubeClient kubernetes.Interface = fake.NewSimpleClientset()
+	kubeConfig := &rest.Config{}
+	config := Config{LookupNamespace: "my-policies"}
+	resolver, _ := NewResolver(&kubeClient, kubeConfig, config)
+
+	_, err := resolver.fromClusterClaim("clusterID")
+
+	if err == nil {
+		t.Fatal("Expecting an error but did not get one")
+	}
+
+	expectedMsg := "fromClusterClaim is not supported because lookups are restricted to the my-policies namespace"
+	if err.Error() != expectedMsg {
+		t.Fatalf(`Expected the error "%s", but got "%s"`, expectedMsg, err.Error())
+	}
+}
