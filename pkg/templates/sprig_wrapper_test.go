8eb3c3e71f1950b4177c26164466c42820953cf2
Dale Haiducek <19750917+dhaiducek@users.noreply.github.com>
2024-04-29 16:51:11 -0400

    Parallelize and alphabetize tests
    Signed-off-by: Dale Haiducek <19750917+dhaiducek@users.noreply.github.com>

--- pkg/templates/sprig_wrapper_test.go
+++ pkg/templates/sprig_wrapper_test.go
@@ -14,277 +14,237 @@ import (
 func TestGetSprigFunc(t *testing.T) {
 	t.Parallel()
 
-	tests := []struct {
-		funcName       string
+	tests := map[string]struct {
 		template       string
 		expectedResult string
 	}{
-		{
-			"trim",
-			`{{ trim "  foo bar  " }}`,
-			"foo bar",
-		},
-		{
-			"upper",
-			`{{ upper "foo bar" }}`,
-			"FOO BAR",
-		},
-		{
-			"lower",
-			`{{ lower "Foo Bar" }}`,
-			"foo bar",
-		},
-		{
-			"contains",
-			`{{ contains "Foo" "Foo Bar" }}`,
-			"true",
-		},
-		{
-			"hasPrefix",
-			`{{ hasPrefix "Foo" "FooBar" }}`,
-			"true",
-		},
-		{
-			"hasSuffix",
-			`{{ hasSuffix "Bar" "FooBar" }}`,
-			"true",
-		},
-		{
-			"quote",
-			`{{ "Foo Bar" | quote }}`,
-			"\"Foo Bar\"",
-		},
-		{
-			"cat",
-			`{{ cat "Foo" "Bar" }}`,
-			"Foo Bar",
-		},
-		{
-			"replace",
-			`{{ "Foo Bar" | replace " " "-" }}`,
-			"Foo-Bar",
-		},
-		{
-			"split",
-			`{{ $a := split "." "api.test.example.com" }}{{ $a._1 }}`,
-			"test",
-		},
-		{
-			"splitn",
-			`{{ $a := splitn "." 3 "api.test.example.com" }}{{ $a._2 }}`,
-			"example.com",
-		},
-		{
-			"list",
-			`{{ list "Foo" "Bar" }}`,
-			"[Foo Bar]",
-		},
-		{
-			"join",
-			`{{ list "Foo" "Bar" | join "_" }}`,
-			"Foo_Bar",
-		},
-		{
-			"until",
-			`{{ until 3 }}`,
-			"[0 1 2]",
-		},
-		{
-			"untilStep",
-			`{{ untilStep 1 6 2 }}`,
-			"[1 3 5]",
-		},
-		{
-			"default",
-			`{{ $a := "Foo Bar" }}{{ default "foo" $a }}`,
-			"Foo Bar",
-		},
-		{
-			"empty",
-			`{{ $a := "Foo Bar" }}{{ empty $a }}`,
-			"false",
-		},
-		{
-			"fromJson",
-			`{{ $a := fromJson "{\"foo\": \"Bar\"}" }}{{ $a.foo }}`,
-			"Bar",
-		},
-		{
-			"mustFromJson",
-			`{{ $a := mustFromJson "{\"foo\": \"Bar\"}" }}{{ $a.foo }}`,
-			"Bar",
-		},
-		{
-			"ternary",
-			`{{ $a := true }}{{ ternary "Foo" "Bar" $a }}`,
-			"Foo",
-		},
-		{
-			"semver",
-			`{{ $a := semver "4.10.22" }}{{ $a.Minor }}`,
-			"10",
-		},
-		{
-			"semverCompare",
-			`{{ semverCompare "^1.2.0" "1.2.3" }}`,
-			"true",
-		},
-		{
-			"toRawJson",
-			`{{ toRawJson .Labels }}`,
-			`{"hello":"world"}`,
-		},
-		{
-			"mustToRawJson",
-			`{{ mustToRawJson .Labels }}`,
-			`{"hello":"world"}`,
-		},
-		{
-			"substr",
-			`{{ substr 0 3 "foo bar" }}`,
-			"foo",
-		},
-		{
-			"trimAll",
-			`{{ trimAll "-" "-foo bar-" }}`,
-			"foo bar",
-		},
-		{
-			"trunc",
-			`{{ trunc 3 "foo bar" }}`,
-			"foo",
-		},
-		{
-			"add",
+		"add": {
 			`{{ add 2 2 }}`,
 			"4",
 		},
-		{
-			"sub",
-			`{{ sub 4 2 }}`,
-			"2",
-		},
-		{
-			"div",
-			`{{ div 4 2 }}`,
-			"2",
-		},
-		{
-			"mul",
-			`{{ mul 2 2 }}`,
-			"4",
-		},
-		{
-			"round",
-			`{{ round 123.55555 0 }}`,
-			"124",
-		},
-		{
-			"toDate",
-			`{{ toDate "2006-01-02" "2023-12-31" | date "01/02/2006" }}`,
-			"12/31/2023",
-		},
-		{
-			"mustToDate",
-			`{{ mustToDate "2006-01-02" "2023-12-31" | date "01/02/2006" }}`,
-			"12/31/2023",
-		},
-		{
-			"append",
+		"append": {
 			`{{ append (list 1 2 3) 4 }}`,
 			"[1 2 3 4]",
 		},
-		{
-			"mustAppend",
-			`{{ mustAppend (list 1 2 3) 4 }}`,
-			"[1 2 3 4]",
+		"cat": {
+			`{{ cat "Foo" "Bar" }}`,
+			"Foo Bar",
 		},
-		{
-			"prepend",
-			`{{ prepend (list 1 2 3) 4 }}`,
-			"[4 1 2 3]",
-		},
-		{
-			"mustPrepend",
-			`{{ mustPrepend (list 1 2 3) 4 }}`,
-			"[4 1 2 3]",
-		},
-		{
-			"concat",
+		"concat": {
 			`{{ concat (list 1 2 3) (list 4 5 6) }}`,
 			"[1 2 3 4 5 6]",
 		},
-		{
-			"has",
-			`{{ has 2 (list 1 2 3) }}`,
+		"contains": {
+			`{{ contains "Foo" "Foo Bar" }}`,
 			"true",
 		},
-		{
-			"mustHas",
-			`{{ mustHas 2 (list 1 2 3) }}`,
-			"true",
+		"default": {
+			`{{ $a := "Foo Bar" }}{{ default "foo" $a }}`,
+			"Foo Bar",
 		},
-		{
-			"slice",
-			`{{ slice (list 1 2 3) 1 3 }}`,
-			"[2 3]",
-		},
-		{
-			"mustSlice",
-			`{{ mustSlice (list 1 2 3) 1 3 }}`,
-			"[2 3]",
-		},
-		{
-			"dig",
+		"dig": {
 			`{{ dig "user" "role" "foo" "default" (fromJson "{\"user\": {\"role\": {\"foo\": \"bar\"}}}") }}`,
 			"bar",
 		},
-
-		{
-			"htpasswd",
+		"div": {
+			`{{ div 4 2 }}`,
+			"2",
+		},
+		"empty": {
+			`{{ $a := "Foo Bar" }}{{ empty $a }}`,
+			"false",
+		},
+		"fromJson": {
+			`{{ $a := fromJson "{\"foo\": \"Bar\"}" }}{{ $a.foo }}`,
+			"Bar",
+		},
+		"has": {
+			`{{ has 2 (list 1 2 3) }}`,
+			"true",
+		},
+		"hasPrefix": {
+			`{{ hasPrefix "Foo" "FooBar" }}`,
+			"true",
+		},
+		"hasSuffix": {
+			`{{ hasSuffix "Bar" "FooBar" }}`,
+			"true",
+		},
+		"htpasswd": {
 			`{{ empty (htpasswd "foo" "bar") }}`,
 			"false",
 		},
+		"join": {
+			`{{ list "Foo" "Bar" | join "_" }}`,
+			"Foo_Bar",
+		},
+		"list": {
+			`{{ list "Foo" "Bar" }}`,
+			"[Foo Bar]",
+		},
+		"lower": {
+			`{{ lower "Foo Bar" }}`,
+			"foo bar",
+		},
+		"mul": {
+			`{{ mul 2 2 }}`,
+			"4",
+		},
+		"mustAppend": {
+			`{{ mustAppend (list 1 2 3) 4 }}`,
+			"[1 2 3 4]",
+		},
+		"mustFromJson": {
+			`{{ $a := mustFromJson "{\"foo\": \"Bar\"}" }}{{ $a.foo }}`,
+			"Bar",
+		},
+		"mustHas": {
+			`{{ mustHas 2 (list 1 2 3) }}`,
+			"true",
+		},
+		"mustPrepend": {
+			`{{ mustPrepend (list 1 2 3) 4 }}`,
+			"[4 1 2 3]",
+		},
+		"mustSlice": {
+			`{{ mustSlice (list 1 2 3) 1 3 }}`,
+			"[2 3]",
+		},
+		"mustToDate": {
+			`{{ mustToDate "2006-01-02" "2023-12-31" | date "01/02/2006" }}`,
+			"12/31/2023",
+		},
+		"mustToRawJson": {
+			`{{ mustToRawJson .Labels }}`,
+			`{"hello":"world"}`,
+		},
+		"prepend": {
+			`{{ prepend (list 1 2 3) 4 }}`,
+			"[4 1 2 3]",
+		},
+		"quote": {
+			`{{ "Foo Bar" | quote }}`,
+			"\"Foo Bar\"",
+		},
+		"replace": {
+			`{{ "Foo Bar" | replace " " "-" }}`,
+			"Foo-Bar",
+		},
+		"round": {
+			`{{ round 123.55555 0 }}`,
+			"124",
+		},
+		"semver": {
+			`{{ $a := semver "4.10.22" }}{{ $a.Minor }}`,
+			"10",
+		},
+		"semverCompare": {
+			`{{ semverCompare "^1.2.0" "1.2.3" }}`,
+			"true",
+		},
+		"slice": {
+			`{{ slice (list 1 2 3) 1 3 }}`,
+			"[2 3]",
+		},
+		"split": {
+			`{{ $a := split "." "api.test.example.com" }}{{ $a._1 }}`,
+			"test",
+		},
+		"splitn": {
+			`{{ $a := splitn "." 3 "api.test.example.com" }}{{ $a._2 }}`,
+			"example.com",
+		},
+		"sub": {
+			`{{ sub 4 2 }}`,
+			"2",
+		},
+		"substr": {
+			`{{ substr 0 3 "foo bar" }}`,
+			"foo",
+		},
+		"ternary": {
+			`{{ $a := true }}{{ ternary "Foo" "Bar" $a }}`,
+			"Foo",
+		},
+		"toDate": {
+			`{{ toDate "2006-01-02" "2023-12-31" | date "01/02/2006" }}`,
+			"12/31/2023",
+		},
+		"toRawJson": {
+			`{{ toRawJson .Labels }}`,
+			`{"hello":"world"}`,
+		},
+		"trim": {
+			`{{ trim "  foo bar  " }}`,
+			"foo bar",
+		},
+		"trimAll": {
+			`{{ trimAll "-" "-foo bar-" }}`,
+			"foo bar",
+		},
+		"trunc": {
+			`{{ trunc 3 "foo bar" }}`,
+			"foo",
+		},
+		"until": {
+			`{{ until 3 }}`,
+			"[0 1 2]",
+		},
+		"untilStep": {
+			`{{ untilStep 1 6 2 }}`,
+			"[1 3 5]",
+		},
+		"upper": {
+			`{{ upper "foo bar" }}`,
+			"FOO BAR",
+		},
 	}
 
-	for _, test := range tests {
-		resolver, _ := NewResolver(k8sConfig, Config{})
+	for funcName, test := range tests {
+		funcName := funcName
+		test := test
 
-		policyYAML := `
+		t.Run(funcName, func(t *testing.T) {
+			t.Parallel()
+
+			resolver, _ := NewResolver(k8sConfig, Config{})
+
+			policyYAML := `
 ---
 data:
   %s: '%s'
 `
 
-		policyJSON, err := yamlToJSON([]byte(fmt.Sprintf(policyYAML, test.funcName, test.template)))
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Failed to convert the policy YAML to JSON: %v\n", err)
-			panic(err)
-		}
+			policyJSON, err := yamlToJSON([]byte(fmt.Sprintf(policyYAML, funcName, test.template)))
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Failed to convert the policy YAML to JSON: %v\n", err)
+				panic(err)
+			}
 
-		templateCtx := struct{ Labels map[string]string }{map[string]string{"hello": "world"}}
+			templateCtx := struct{ Labels map[string]string }{map[string]string{"hello": "world"}}
 
-		resolvedResult, err := resolver.ResolveTemplate(policyJSON, templateCtx, nil)
-		if err != nil {
-			t.Fatalf("Failed to process the policy YAML: %v\n", err)
-		}
+			resolvedResult, err := resolver.ResolveTemplate(policyJSON, templateCtx, nil)
+			if err != nil {
+				t.Fatalf("Failed to process the policy YAML: %v\n", err)
+			}
 
-		policyResolvedJSON := resolvedResult.ResolvedJSON
-		var policyResolved interface{}
-		err = yaml.Unmarshal(policyResolvedJSON, &policyResolved)
+			policyResolvedJSON := resolvedResult.ResolvedJSON
+			var policyResolved interface{}
+			err = yaml.Unmarshal(policyResolvedJSON, &policyResolved)
 
-		data, ok := policyResolved.(map[string]interface{})["data"].(map[string]interface{})
-		if !ok {
-			t.Fatalf("Failed to process the policy YAML reading data key: %v\n", err)
-		}
+			data, ok := policyResolved.(map[string]interface{})["data"].(map[string]interface{})
+			if !ok {
+				t.Fatalf("Failed to process the policy YAML reading data key: %v\n", err)
+			}
 
-		actualValue, ok := data[test.funcName].(string)
-		if !ok {
-			t.Fatalf("Failed testing %s: %v\n", test.funcName, err)
-		}
+			actualValue, ok := data[funcName].(string)
+			if !ok {
+				t.Fatalf("Failed testing %s: %v\n", funcName, err)
+			}
 
-		if actualValue != test.expectedResult {
-			t.Fatalf("Test %s failed. expected : %v , got : %v", test.funcName, test.expectedResult, actualValue)
-		}
+			if actualValue != test.expectedResult {
+				t.Fatalf("Test %s failed. expected : %v , got : %v", funcName, test.expectedResult, actualValue)
+			}
+		})
 	}
 }
