c861dc30e4a0bc0007578022793568114fb7895d
Tomas Silva <136352470+tomilho@users.noreply.github.com>
2026-08-08 11:36:24 +0100

    fix: handle error in processRawGoTemplate in SkipPolicyValidation
    Signed-off-by: Tomas Silva <136352470+tomilho@users.noreply.github.com>

--- cmd/template-resolver/utils/resolver_utils.go
+++ cmd/template-resolver/utils/resolver_utils.go
@@ -281,7 +281,11 @@ func (t *TemplateResolver) ProcessTemplate(yamlBytes []byte) ([]byte, error) {
 	default:
 		if t.SkipPolicyValidation {
 			var resolvedRaw any
+
 			resolvedRaw, err = processRawGoTemplate(string(yamlBytes), resolver, tempCtx)
+			if err != nil {
+				return nil, err
+			}
 
 			if resolved, ok := resolvedRaw.(map[string]interface{}); ok {
 				policy.Object = resolved
